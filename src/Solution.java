896afa3ddf569ddb7f79e83eb48a4f9bfecf835d
wuqingc <xuan1c2@gmail.com>
2024-04-17 17:10:07 +0800

    Changes

--- src/Solution.java
+++ src/Solution.java
@@ -6,6 +6,21 @@ import java.util.*;
  */
 class Solution {
 
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, ans = 0;
+
+        while (left < right) {
+            ans = Math.max(ans,
+                    (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
+
 
     public static void main(String[] args) {
     }
